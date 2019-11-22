2299b5092096e1ba2e63eb33adc079a1230642ac
Alejandro Felipe Paredes <motoralfa.10.11.12@gmail.com>
2019-11-22 04:25:03 -0300

    las update

--- sniffer.py
+++ sniffer.py
@@ -5,16 +5,18 @@ print("########## UDP Sniffer ##########\n")
 print("### Configuracion de entrada. ###\n")
 ip = input('IP de entrada: ')
 port = input('Puerto de entrada: ')
-i = input('Interface de entrada: ')
+# i = input('Interface de entrada: ')
 print("\n#### Configuracion de salida ####\n")
 ipout =input('IP de salida: ')
 portout = input('Puerto de salida: ')
-iout = input('Interface de salida: ')
+# iout = input('Interface de salida: ')
 print("\n### Escuchando el puerto "+port+" ###")
 print("\n### Reenviando al puerto "+portout+" ###\n")
 
 def myFunction(pkt):
-	send((IP(dst=ipout,src=ip)/UDP(dport=int(portout))/pkt[Raw].load),iface=i)
-	print(pkt[Raw].load)
-
-packets = sniff(iface=iout, filter='udp and port '+port, prn=myFunction)
+	if Raw in pkt:
+		send((IP(dst=ipout,src=ip)/UDP(dport=int(portout))/pkt[Raw].load))
+		print(pkt[Raw].load)
+	else:
+		print(pkt)
+packets = sniff(filter='udp and port '+port, prn=myFunction)
